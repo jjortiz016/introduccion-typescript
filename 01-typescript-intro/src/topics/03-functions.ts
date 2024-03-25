3375dc56eb056b84727433a637ac8a7400dddabd
jjortiz016 <jjortiz016@misena.edu.co>
2024-03-25 16:59:59 -0500

    Funciones con objetos como argumentos.

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -14,10 +14,33 @@ function multiply(firstNumber: number,secondNumber?: number, base: number=2){ //
     return firstNumber*base;
 }
 
-const result: number= addNumbers(1,2)
+//const result: number= addNumbers(1,2)
+//const result2: string= addNumbersArrow(2,2);
+//const multiplyResult: number = multiply(5);
 
-const result2: string= addNumbersArrow(2,2);
-const multiplyResult: number = multiply(5);
+//console.log({result,result2,multiplyResult}); // coloco la variable entre {} para que se imprima como un objeto.
 
-console.log({result,result2,multiplyResult}); // coloco la variable entre {} para que se imprima como un objeto.
+interface Character {
+    name:string;
+    hp: number;
+    showHp:() =>void;
+}
 
+const healCharacter= (character: Character, amount: number )=>{
+   // CharacterData.pv+=amount;  //como la propiedad no existe por no estar definida en la interface marcara error
+       character.hp+=amount;
+      
+}
+
+const strider: Character={
+    name: 'Strider',
+    hp: 50,
+    showHp(){
+        console.log(`Puntos de vida ${this.hp}`)
+    }
+}
+
+healCharacter(strider,10)
+healCharacter(strider,20)
+
+strider.showHp();
