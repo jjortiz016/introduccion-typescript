8507c6aaa194f6aea6ad443ef728ebc4eb5017ec
jjortiz016 <jjortiz016@misena.edu.co>
2024-03-25 15:46:13 -0500

    tipos basicos y conceptos generales.

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,10 +1,10 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+import './topics/01-basic-.ts'
+
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
  Hola mundo
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
---
+++ 01-typescript-intro/src/topics/01-basic-.ts
@@ -0,0 +1,8 @@
+const name = 'jhon'; //si sabemos que no va a cambia le ponemos const
+let hpPoinst: number | 'FULL' =95;
+const isAlive: boolean= true;
+
+hpPoinst= 'FULL'
+
+console.log({name, hpPoinst, isAlive});
+export{}
