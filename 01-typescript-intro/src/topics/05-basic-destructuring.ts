64a01f8b85136c2035969e07315f6560279a042e
jjortiz016 <jjortiz016@misena.edu.co>
2024-03-26 16:21:30 -0500

    Desestructuracion de arreglos.

--- 01-typescript-intro/src/topics/05-basic-destructuring.ts
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -26,14 +26,25 @@ const audioPlayer: AudioPlayer={
 const {song: anotherSong, songDuration:duration, details}= audioPlayer;
 const{author} = details;
 /************ */
-const song =' New Song';
-console.log('Song: ', audioPlayer.song);
-console.log('Song: ',song);
-console.log('Song: ',anotherSong);
-console.log('Song: ',duration);
-console.log('Duration : ', audioPlayer.songDuration);
-console.log('Author: ', audioPlayer.details.author);
-console.log('Author: ', author);
+//const song =' New Song';
+//console.log('Song: ', audioPlayer.song);
+//console.log('Song: ',song);
+//console.log('Song: ',anotherSong);
+//console.log('Song: ',duration);
+//console.log('Duration : ', audioPlayer.songDuration);
+//console.log('Author: ', audioPlayer.details.author);
+//console.log('Author: ', author);
+
+
+/****DESESTRUCTURACION DE ARREGLOS */
+const dbz: string[]= ['Goku', 'Vegeta','Trunks'];
+
+console.error('Personaje 3:', dbz[2] || 'No ha personaje')
+
+//SEGUNDO EJEMPLO
+
+const [,,trunks='Not found']:string[]= ['Goku','Vegeta']
+console.error('Personaje 3:', trunks); //saldra el mensaje Not found
 
 export{
 
