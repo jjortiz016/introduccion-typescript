cedc08b19355851484b9792024b8de0a2a54efd7
jjortiz016 <jjortiz016@misena.edu.co>
2024-03-25 20:05:23 -0500

    Tarea sobre objetos e interfaces.

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,8 +1,8 @@
 import './style.css'
-import './topics/01-basic-.ts'
-import './topics/02-object-interface.ts'
-import './topics/03-functions.ts'
-
+//import './topics/01-basic-.ts'
+//import './topics/02-object-interface.ts'
+//import './topics/03-functions.ts'
+import './topics/04-homework-types';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
  Hola mundo
 `
