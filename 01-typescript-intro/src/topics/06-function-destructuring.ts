8cc62fef29cb7ff5bcfad3ddb0130b18813f650c
jjortiz016 <jjortiz016@misena.edu.co>
2024-03-28 20:38:01 -0500

    Desustructuración de argumentos

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,64 @@
+
+interface Product{
+    description:string;
+    price:number;
+}
+
+const phone: Product={
+    description: 'Nokia A1',
+    price: 150.0
+}
+
+const tablet: Product = {
+    description: 'iPad Air',
+    price: 250.0
+}
+
+interface TaxtCalculationOptions{
+    tax: number;
+    products:Product[];
+}
+
+//function taxCalculation(options: TaxtCalculationOptions): number[]{  //desustructurar argumentos
+//function taxCalculation({tax,products}: TaxtCalculationOptions):[number,number]{ //[number,number] devulve una tupla
+function taxCalculation(options: TaxtCalculationOptions):[number,number]{ //[number,number] devulve una tupla
+    let total =0;
+    const {tax,products}=options;
+    //options.products.forEach(product=>{
+       // products.forEach(product=>{
+        //tarea desustructurar el objeto product
+       products.forEach(({price}) => { //tercera forma de desustrucuturar para sacar el precio
+       // total += product.price;   //desustructurar ya que lo unico que se necesita es el precio
+         //utilizamos el precio desustrucuturado
+         total+=price;
+    });
+   // return [total, total*options.tax];
+   return [total, total*tax];
+}
+
+const shoppingCart =  [phone, tablet];
+const tax=0.15;
+
+const result = taxCalculation({ //desusctruturar resultado que es un arreglo
+    products: shoppingCart,
+    tax:tax   // tambien puede dejarse un solo tax ya que la propiedad y la variable se llaman igual.
+})
+
+/*alternativa de desustructuracion
+const [res,tax1] = taxCalculation({ 
+    products: shoppingCart,
+    tax:tax   
+})
+
+*/
+
+
+
+/*tarea vamos a desustrucurar result que es un arreglo*/
+const [res,tax1]=[result[0], result[1]]
+
+//console.log('Total', result[0]);
+//console.log('Tax', result[1]);
+console.log('Total', res);
+console.log('Tax', tax1);
+export{}
